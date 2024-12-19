62755b25d2f07e9d285e80bc23078da6788e049c
egetelli <egetelli@hotmail.com>
2024-12-19 14:25:37 +0300

    Finishing the app and Maxby and Minby methods

--- SWAPI/Program.cs
+++ SWAPI/Program.cs
@@ -60,6 +60,63 @@ public class StarWarsPlanetsStatsApp
         {
             Console.WriteLine(planet);
         }
+
+        Console.WriteLine();
+        Console.WriteLine(
+            "The statistics of which property would you " +
+            "like to see?");
+        Console.WriteLine("population");
+        Console.WriteLine("diameter");
+        Console.WriteLine("surface water");
+
+        var userChoice = Console.ReadLine();
+
+        if(userChoice == "population")
+        {
+            ShowStatistics(
+                planets,
+                "population",
+                planet => planet.Population);            
+        }
+        else if (userChoice == "diameter")
+        {
+            ShowStatistics(
+                planets,
+                "diameter",
+                planet => planet.Diameter);
+        }
+        else if (userChoice == "surface water")
+        {
+            ShowStatistics(
+                planets,
+                "surface water",
+                planet => planet.SurfaceWater);
+        }
+        else
+        {
+            Console.WriteLine("Invalid choice");
+        }
+
+    }
+
+    private void ShowStatistics(
+        IEnumerable<Planet> planets,
+        string propertyName,
+        Func<Planet, int?> propertySelector)
+    {
+        var planetWithMaxPropertyValue =
+            planets.MaxBy(propertySelector);
+
+        Console.WriteLine($"Max {propertyName} is: " +
+            $"{propertySelector(planetWithMaxPropertyValue)} " +
+            $"(planet: {planetWithMaxPropertyValue.Name}) ");
+
+        var planetWithMinPropertyValue =
+            planets.MinBy(propertySelector);
+
+        Console.WriteLine($"Min {propertyName} is: " +
+            $"{propertySelector(planetWithMinPropertyValue)} " +
+            $"(planet: {planetWithMinPropertyValue.Name}) ");
     }
 
     private IEnumerable<Planet> ToPlanets(Root? root)
